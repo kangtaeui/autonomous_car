12296af51a05712b3daae60ac76ebc6d2d261beb
kangtaeui <141004174+kangtaeui@users.noreply.github.com>
2023-12-19 16:23:09 +0900

    Update ultrasonic.h

--- ultrasonic.h
+++ ultrasonic.h
@@ -1,13 +1,11 @@
-#ifndef ULTRASOUND_H
-#define ULTRASOUND_H
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
 
 #include <pigpio.h>
-#include <pthread.h>
 
 class UltraSound {
 public:
-    UltraSound(unsigned int trigger, unsigned int echo, int sensorNumber, double xCoordinate);
-    void Init();
+    void Init(unsigned int trigger, unsigned int echo, int sensorNumber, double xCoordinate);
     double GetDistance(unsigned int timeout);
     double GetHorizontalDistance(const UltraSound& otherSensor) const;
     int GetSensorNumber() const;
@@ -20,14 +18,22 @@ private:
     int echo;
     int sensorNumber;
     double xCoordinate;
+
     volatile long startTimeUsec;
     volatile long endTimeUsec;
+
     double distanceCm;
     double previousDistanceCm;
     long DifferenceTimeUsec;
     long now;
+
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
     static const double DISTANCE_CONVERSION_FACTOR;
 };
 
-#endif
+void AdjustSpeedBasedOnDistance(double distance, int& speed);
+
+double CalculateAngle(double distance1, double distance3);
+
+#endif // ULTRASONIC_H

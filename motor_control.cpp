734674a87186e784f5e95f948ca65c99ab79e351
kangtaeui <141004174+kangtaeui@users.noreply.github.com>
2024-01-22 22:30:02 +0900

    Update motor_control.cpp

--- motor_control.cpp
+++ motor_control.cpp
@@ -1,52 +1,54 @@
 #include "motor_control.h"
-#include <pigpio.h>
-//하나하나 맵핑하여 어떻게 동작할지 넣어주기
+#include <pigpio.h>    
+
+//Drive forward
 void Driving_forward(int in1, int in2, int in3, int in4, int speed)
 {
-    gpioWrite(in1, 0);
-    gpioWrite(in2, 1);
-    gpioWrite(in3, 0);
-    gpioWrite(in4, 1);
-    gpioPWM(ENA, speed);
-    gpioPWM(ENB, speed);
+    //in1 and in2 are a set of signals on the wheel, so high or low signals must be set
+    gpioWrite(in1, 0);    //right side's in1 signal is low    
+    gpioWrite(in2, 1);    //right side's in2 signal is high
+    gpioWrite(in3, 0);    //left side's in3 signal is low
+    gpioWrite(in4, 1);    //left side's in4 signal is high
+    gpioPWM(ENA, speed);    //enableA takes a duty
+    gpioPWM(ENB, speed);    //enableB takes a duty
 }
-
+//Drive backward
 void Driving_backward(int in1, int in2, int in3, int in4, int speed)
 {
-    gpioWrite(in1, 1);
-    gpioWrite(in2, 0);
-    gpioWrite(in3, 1);
-    gpioWrite(in4, 0);
-    gpioPWM(ENA, speed);
-    gpioPWM(ENB, speed);
+    gpioWrite(in1, 1);    //right side's in1 signal is high
+    gpioWrite(in2, 0);    //right side's in2 signal is low
+    gpioWrite(in3, 1);    //left side's in3 signal is high
+    gpioWrite(in4, 0);    //left side's in4 signal is low
+    gpioPWM(ENA, speed);    //enableA takes a duty
+    gpioPWM(ENB, speed);    //enableB takes a duty
 }
-
+//Drive left
 void Driving_left(int in1, int in2, int in3, int in4, int speed)
 {
-    gpioWrite(in1, 0);
-    gpioWrite(in2, 1);
-    gpioWrite(in3, 0);
-    gpioWrite(in4, 0);
-    gpioPWM(ENA, speed);
-    gpioPWM(ENB, speed);
-}
-
+    gpioWrite(in1, 0);    //right side's in1 signal is low
+    gpioWrite(in2, 1);    //right side's in2 signal is high
+    gpioWrite(in3, 0);    //left side's in3 signal is low
+    gpioWrite(in4, 0);    //left side's in4 signal is low
+    gpioPWM(ENA, speed);    //enableA takes a duty
+    gpioPWM(ENB, speed);    //enableB takes a duty
+}    
+//Drive right
 void Driving_right(int in1, int in2, int in3, int in4, int speed)
 {
-    gpioWrite(in1, 0);
-    gpioWrite(in2, 0);
-    gpioWrite(in3, 0);
-    gpioWrite(in4, 1);
-    gpioPWM(ENA, speed);
-    gpioPWM(ENB, speed);
+    gpioWrite(in1, 0);    //right side's in1 signal is low
+    gpioWrite(in2, 0);    //right side's in2 signal is low
+    gpioWrite(in3, 0);    //left side's in3 signal is low
+    gpioWrite(in4, 1);    //left side's in4 signal is high
+    gpioPWM(ENA, speed);    //enableA takes a duty
+    gpioPWM(ENB, speed);    //enableB takes a duty
 }
-
-void Driving_stop(int in1, int in2, int in3, int in4)
+//Drive stop so i will set the speed 0.
+void Driving_stop(int in1, int in2, int in3, int in4, int speed)
 {
-    gpioWrite(in1, 1);
-    gpioWrite(in2, 1);
-    gpioWrite(in3, 1);
-    gpioWrite(in4, 1);
-    gpioPWM(ENA, speed);
-    gpioPWM(ENB, speed);
+    gpioWrite(in1, 1);    //right side's in1 signal is high
+    gpioWrite(in2, 1);    //right side's in1 signal is high
+    gpioWrite(in3, 1);    //left side's in4 signal is high
+    gpioWrite(in4, 1);    //left side's in4 signal is high, this direction is None, because all signal is high
+    gpioPWM(ENA, speed);    //0
+    gpioPWM(ENB, speed);    //0
 }
